a6738c88d8957cf5fd6e37a93cce10be598d7786
edgarriba <edgar.riba@gmail.com>
2017-02-15 23:49:03 +0100

    remove kDim parameter and add strided subView

--- tiny_dnn/core/framework/tensor_utils.h
+++ tiny_dnn/core/framework/tensor_utils.h
@@ -57,7 +57,7 @@
 
 namespace tiny_dnn {
 
-template <typename T>
+/*template <typename T>
 void print_pack(std::ostream &out, T t) {  // TODO: C++17 allows easier printing
   out << t;
 }
@@ -68,11 +68,11 @@ void print_pack(std::ostream &out, T t, U u, Args... args) {
   print_pack(out, u, args...);
 }
 
-template <typename T, size_t kDim, typename... Args>
+template <typename T, typename... Args>
 inline std::ostream &print_last_two_dimesions(std::ostream &os,
-                                              const Tensor<T, kDim> &tensor,
+                                              const Tensor<T> &tensor,
                                               const Args... args) {
-  const std::array<size_t, kDim> &shape = tensor.shape();
+  const std::array<size_t> &shape = tensor.shape();
   for (size_t k = 0; k < shape[kDim - 2]; ++k) {
     for (size_t l = 0; l < shape[kDim - 1]; ++l) {
       os << "\t" << tensor.host_at(args..., k, l);
@@ -83,14 +83,13 @@ inline std::ostream &print_last_two_dimesions(std::ostream &os,
 }
 
 template <typename T,
-          size_t kDim,
           typename... Args,
           typename std::enable_if<sizeof...(Args) == kDim - 3, int>::type = 0>
 inline std::ostream &print_last_n_dimesions(std::ostream &os,
-                                            const Tensor<T, kDim> &tensor,
+                                            const Tensor<T> &tensor,
                                             const int d,
                                             const Args... args) {
-  // const std::array<size_t, kDim>& shape = tensor.shape();
+  // const std::array<size_t>& shape = tensor.shape();
   // const size_t n_dim = sizeof...(args);
   os << "Tensor(";
   print_pack(os, d, args...);
@@ -100,14 +99,13 @@ inline std::ostream &print_last_n_dimesions(std::ostream &os,
 }
 
 template <typename T,
-          size_t kDim,
           typename... Args,
           typename std::enable_if<(sizeof...(Args) < kDim - 3), int>::type = 0>
 inline std::ostream &print_last_n_dimesions(std::ostream &os,
-                                            const Tensor<T, kDim> &tensor,
+                                            const Tensor<T> &tensor,
                                             const int d,
                                             const Args... args) {
-  const std::array<size_t, kDim> &shape = tensor.shape();
+  const std::array<size_t> &shape = tensor.shape();
   const size_t n_dim = sizeof...(args);
   for (size_t k = 0; k < shape[n_dim + 1]; ++k) {
     print_last_n_dimesions(os, tensor, d, args..., k);
@@ -128,7 +126,7 @@ template <typename T>
 inline std::ostream &operator<<(std::ostream &os, const Tensor<T, 2> &tensor) {
   print_last_two_dimesions(os, tensor);
   return os;
-}
+}*/
 
 /**
  * Overloaded method to print the Tensor class to the standard output
@@ -136,20 +134,20 @@ inline std::ostream &operator<<(std::ostream &os, const Tensor<T, 2> &tensor) {
  * @param tensor
  * @return
  */
-template <typename T, size_t kDim>
+/*template <typename T>
 inline std::ostream &operator<<(std::ostream &os,
-                                const Tensor<T, kDim> &tensor) {
-  const std::array<size_t, kDim> &shape = tensor.shape();
+                                const Tensor<T> &tensor) {
+  const std::array<size_t> &shape = tensor.shape();
   for (size_t i = 0; i < shape[0]; ++i) print_last_n_dimesions(os, tensor, i);
   return os;
-}
+}*/
 
 // utilities for element-wise and tensor-scalar/scalar-tensor operations
 
-template <typename TD, typename TS1, typename TS2, typename F, size_t kDim>
-void binary_tensor_tensor_elementwise_operation(Tensor<TD, kDim> &dst,
-                                                const Tensor<TS1, kDim> &src1,
-                                                const Tensor<TS2, kDim> &src2,
+template <typename TD, typename TS1, typename TS2, typename F>
+void binary_tensor_tensor_elementwise_operation(Tensor<TD> &dst,
+                                                const Tensor<TS1> &src1,
+                                                const Tensor<TS2> &src2,
                                                 F f) {
   if (src1.shape() != src2.shape()) {
     throw nn_error("Tensor must have same shape");
@@ -166,9 +164,9 @@ void binary_tensor_tensor_elementwise_operation(Tensor<TD, kDim> &dst,
   });
 }
 
-template <typename TD, typename TS, typename F, size_t kDim>
-void unary_tensor_elementwise_operation(Tensor<TD, kDim> &dst,
-                                        const Tensor<TS, kDim> &src,
+template <typename TD, typename TS, typename F>
+void unary_tensor_elementwise_operation(Tensor<TD> &dst,
+                                        const Tensor<TS> &src,
                                         F f) {
   dst.resize(src.shape());
 
@@ -178,9 +176,9 @@ void unary_tensor_elementwise_operation(Tensor<TD, kDim> &dst,
   for_i(true, dst.size(), [pdst, psrc, &f](size_t i) { pdst[i] = f(psrc[i]); });
 }
 
-template <typename TD, typename TS1, typename TS2, typename F, size_t kDim>
-void binary_tensor_scalar_operation(Tensor<TD, kDim> &dst,
-                                    const Tensor<TS1, kDim> &src1,
+template <typename TD, typename TS1, typename TS2, typename F>
+void binary_tensor_scalar_operation(Tensor<TD> &dst,
+                                    const Tensor<TS1> &src1,
                                     TS2 src2,
                                     F f) {
   dst.resize(src1.shape());
@@ -192,10 +190,10 @@ void binary_tensor_scalar_operation(Tensor<TD, kDim> &dst,
         [pdst, psrc1, src2, &f](size_t i) { pdst[i] = f(psrc1[i], src2); });
 }
 
-template <typename TD, typename TS1, typename TS2, typename F, size_t kDim>
-void binary_scalar_tensor_operation(Tensor<TD, kDim> &dst,
+template <typename TD, typename TS1, typename TS2, typename F>
+void binary_scalar_tensor_operation(Tensor<TD> &dst,
                                     TS1 src1,
-                                    const Tensor<TS2, kDim> &src2,
+                                    const Tensor<TS2> &src2,
                                     F f) {
   dst.resize(src2.shape());
 
@@ -251,70 +249,70 @@ T exp(T s1) {
 
 }  // namespace details
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_add(Tensor<TD, kDim> &dst, TS1 src1, const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_add(Tensor<TD> &dst, TS1 src1, const Tensor<TS2> &src2) {
   binary_scalar_tensor_operation(dst, src1, src2, details::plus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_add(Tensor<TD, kDim> &dst, const Tensor<TS1, kDim> &src1, TS2 src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_add(Tensor<TD> &dst, const Tensor<TS1> &src1, TS2 src2) {
   binary_tensor_scalar_operation(dst, src1, src2, details::plus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_add(Tensor<TD, kDim> &dst,
-               const Tensor<TS1, kDim> &src1,
-               const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_add(Tensor<TD> &dst,
+               const Tensor<TS1> &src1,
+               const Tensor<TS2> &src2) {
   binary_tensor_tensor_elementwise_operation(dst, src1, src2,
                                              details::plus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_sub(Tensor<TD, kDim> &dst, TS1 src1, const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_sub(Tensor<TD> &dst, TS1 src1, const Tensor<TS2> &src2) {
   binary_scalar_tensor_operation(dst, src1, src2, details::minus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_sub(Tensor<TD, kDim> &dst, const Tensor<TS1, kDim> &src1, TS2 src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_sub(Tensor<TD> &dst, const Tensor<TS1> &src1, TS2 src2) {
   binary_tensor_scalar_operation(dst, src1, src2, details::minus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_sub(Tensor<TD, kDim> &dst,
-               const Tensor<TS1, kDim> &src1,
-               const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_sub(Tensor<TD> &dst,
+               const Tensor<TS1> &src1,
+               const Tensor<TS2> &src2) {
   binary_tensor_tensor_elementwise_operation(dst, src1, src2,
                                              details::minus<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_mul(Tensor<TD, kDim> &dst, TS1 src1, const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_mul(Tensor<TD> &dst, TS1 src1, const Tensor<TS2> &src2) {
   binary_scalar_tensor_operation(dst, src1, src2,
                                  details::multiplies<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_mul(Tensor<TD, kDim> &dst, const Tensor<TS1, kDim> &src1, TS2 src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_mul(Tensor<TD> &dst, const Tensor<TS1> &src1, TS2 src2) {
   binary_tensor_scalar_operation(dst, src1, src2,
                                  details::multiplies<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_mul(Tensor<TD, kDim> &dst,
-               const Tensor<TS1, kDim> &src1,
-               const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_mul(Tensor<TD> &dst,
+               const Tensor<TS1> &src1,
+               const Tensor<TS2> &src2) {
   binary_tensor_tensor_elementwise_operation(dst, src1, src2,
                                              details::multiplies<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_div(Tensor<TD, kDim> &dst, TS1 src1, const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_div(Tensor<TD> &dst, TS1 src1, const Tensor<TS2> &src2) {
   binary_scalar_tensor_operation(dst, src1, src2,
                                  details::divides_checked<TS1, TS2>);
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_div(Tensor<TD, kDim> &dst, const Tensor<TS1, kDim> &src1, TS2 src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_div(Tensor<TD> &dst, const Tensor<TS1> &src1, TS2 src2) {
   if (src2 == TS2(0.0)) {
     dst.resize(src1.shape());
     dst.fill(std::numeric_limits<TD>::quiet_NaN());
@@ -324,22 +322,22 @@ void layer_div(Tensor<TD, kDim> &dst, const Tensor<TS1, kDim> &src1, TS2 src2) {
   }
 }
 
-template <typename TD, typename TS1, typename TS2, size_t kDim>
-void layer_div(Tensor<TD, kDim> &dst,
-               const Tensor<TS1, kDim> &src1,
-               const Tensor<TS2, kDim> &src2) {
+template <typename TD, typename TS1, typename TS2>
+void layer_div(Tensor<TD> &dst,
+               const Tensor<TS1> &src1,
+               const Tensor<TS2> &src2) {
   binary_tensor_tensor_elementwise_operation(
     dst, src1, src2, details::divides_checked<TS1, TS2>);
 }
 
-template <typename TD, typename TS, size_t kDim>
-void layer_sqrt(Tensor<TD, kDim> &dst, const Tensor<TS, kDim> &src1) {
+template <typename TD, typename TS>
+void layer_sqrt(Tensor<TD> &dst, const Tensor<TS> &src1) {
   return unary_tensor_elementwise_operation(dst, src1,
                                             details::sqrt_checked<TS>);
 }
 
-template <typename TD, typename TS, size_t kDim>
-void layer_exp(Tensor<TD, kDim> &dst, const Tensor<TS, kDim> &src1) {
+template <typename TD, typename TS>
+void layer_exp(Tensor<TD> &dst, const Tensor<TS> &src1) {
   return unary_tensor_elementwise_operation(dst, src1, details::exp<TS>);
 }
 
